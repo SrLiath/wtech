6eaf5d0aaf06fcd17fbe9e87aa425347ac1191bf
SrLiath <86482642+SrLiath@users.noreply.github.com>
2022-11-17 14:27:44 -0300

    Delete desafiotech directory

--- desafiotech/wtech/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path ('registrar/', views.registrar, name='registrar'),
-    path ('tela/', views.tela, name='tela'),
-    path ('export_csv', views.export_csv, name='export_csv'),
-]
